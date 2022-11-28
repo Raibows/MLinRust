19f87a9375b7071a555ecfd968a06121346affab
raibows <raibows@hotmail.com>
2022-11-28 13:56:16 +0800

    update Linear Regression Model

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -2,6 +2,7 @@ use crate::dataset::TaskLabelType;
 pub mod decision_tree;
 pub mod logistic_regression;
 pub mod utils;
+pub mod linear_regression;
 
 pub trait Model<T: TaskLabelType> {
     // todo implement generics for feature to accept batch or ndarray etc.
