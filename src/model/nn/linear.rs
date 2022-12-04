767fb02ca8cc20faa857267df82b73e22e12c6ee
workchi <raibows@hotmail.com>
2022-12-04 23:41:59 +0800

    refactor nn linear module

--- src/model/nn/linear.rs
+++ src/model/nn/linear.rs
@@ -1,6 +1,7 @@
-use crate::ndarray::{NdArray};
+use crate::{ndarray::{NdArray}, model::utils::{Penalty, NormType, calculate_penalty_grad, gradient_clip}};
 
 use super::NNBackPropagation;
+
 
 /// Linear is a nn module, similar to torch.nn.Linear
 pub struct Linear {
@@ -11,6 +12,7 @@ pub struct Linear {
     // current node gradient
     temp_grad_w: Option<NdArray>,
     temp_grad_b: Option<NdArray>,
+    penalty: Option<Penalty>
 }
 
 impl NNBackPropagation for Linear {
@@ -22,7 +24,7 @@ impl NNBackPropagation for Linear {
 
         // calculate current node gradient
         if required_grad {
-            self.calculate_current_node_gradient(input);
+            self.save_grad_graph(input);
         }
 
         outputs
@@ -31,12 +33,38 @@ impl NNBackPropagation for Linear {
     fn backward(&mut self, bp_grad: &NdArray) -> NdArray {
         // bp [bsz, outsize]
         // temp [bsz, insize]
-        self.grad_w = bp_grad.permute(vec![1, 0]) * &self.temp_grad_w.take().unwrap();
+        // println!("first loss {}", bp_grad);
 
-        self.grad_b = &self.temp_grad_b.take().unwrap() * bp_grad;
-        self.grad_b.squeeze(0);
+        self.grad_w += bp_grad.permute(vec![1, 0]) * &self.temp_grad_w.take().unwrap();
+
+        let mut temp_grad_b = &self.temp_grad_b.take().unwrap() * bp_grad;
+        temp_grad_b.squeeze(0);
+        self.grad_b += temp_grad_b;
 
         bp_grad * &self.weight
+    }
+
+    fn step(&mut self, reduction: usize, lr: f32, gradient_clip_by_norm: Option<NormType>) {
+
+        if self.penalty.is_some() {
+            self.grad_w += calculate_penalty_grad(&self.weight, self.penalty.unwrap());
+        }
+
+        self.grad_w /= reduction as f32;
+        self.grad_b /= reduction as f32;
+
+        if gradient_clip_by_norm.is_some() {
+            gradient_clip(&mut self.grad_w, gradient_clip_by_norm.as_ref().unwrap());
+            gradient_clip(&mut self.grad_b, gradient_clip_by_norm.as_ref().unwrap());
+        }
+        // println!("weight grad {}", self.grad_w);
+        // println!("bias grad {}", self.grad_b);
+
+        self.weight -= &self.grad_w * lr;
+        self.bias -= &self.grad_b * lr;
+
+        self.grad_w.clear();
+        self.grad_b.clear();
     }
 }
 
@@ -47,18 +75,29 @@ impl Linear {
     /// insize: m
     /// 
     /// outsize: n
-    pub fn new(insize: usize, outsize: usize) -> Self {
+    pub fn new(insize: usize, outsize: usize, penalty: Option<Penalty>) -> Self {
         let weight = NdArray::new(vec![outsize, insize]);
         let bias = NdArray::new(vec![outsize]);
-        Self { weight: weight.clone(), bias: bias.clone(), grad_w: weight, grad_b: bias, temp_grad_w: None, temp_grad_b: None }
+        Self { weight: weight.clone(), bias: bias.clone(), grad_w: weight, grad_b: bias, temp_grad_w: None, temp_grad_b: None, penalty: penalty }
     }
 
-    fn calculate_current_node_gradient(&mut self, input: &NdArray) {
+    fn save_grad_graph(&mut self, input: &NdArray) {
         // input [bsz, hidden]
         // no average here. should I take average of the gradient for every step? or just in the final step?
 
         self.temp_grad_w = Some(input.clone());
         self.temp_grad_b = Some(NdArray::new(vec![vec![1.0; input.shape[0]]]));
+    }
+
+    pub fn weight_init<F>(&mut self, init_fn: F) where F: Fn(&mut [f32]) {
+        init_fn(self.weight.data_as_mut_vector());
+    }
+
+    pub fn forward_as_borrow(&self, input: &NdArray) -> NdArray {
+        // input should be [bsz, hidden]
+        
+        // forward to get outputs
+        input * &self.weight.permute(vec![1, 0])  + &self.bias
     }
 }
 
@@ -66,31 +105,42 @@ impl Linear {
 mod test {
     use std::vec;
 
-    use crate::ndarray::NdArray;
+    use crate::{ndarray::NdArray, model::{utils::Penalty, nn::criterion::MeanSquaredError}};
 
     use super::{Linear, NNBackPropagation};
 
     #[test]
     fn test_back_propagation() {
-        let mut x = NdArray::new(vec![1.0; 12]);
-        let mut target = NdArray::new(vec![-1.0; 4]);
+        let mut x = NdArray::new(vec![3.5; 12]);
+        let target = vec![-1.0; 4];
         x.reshape(vec![4usize, 3]);
-        target.reshape(vec![4, -1]);
         
-        let mut l1 = Linear::new(3, 5);
-        let mut l2 = Linear::new(5, 1);
+        let mut l1 = Linear::new(3, 5, None);
+        let mut l2 = Linear::new(5, 1, None);
+        for _ in 0..5 {
+            let mut criterion = MeanSquaredError::new();
+            let outs = l1.forward(&x, true);
+            println!("out1 {}", outs);
+            let outs = l2.forward(&outs, true);
+            println!("out2 {}", outs);
 
-        let outs = l1.forward(&x, true);
-        println!("out1 {}", outs);
-        let outs = l2.forward(&outs, true);
-        println!("out2 {}", outs);
+            let loss = criterion.forward(outs, &target);
+            println!("loss {}", loss);
+            let bp_grad = l2.backward(&loss);
+            println!("bp_grad1 {}", bp_grad);
+            let bp_grad = l1.backward(&bp_grad);
+            println!("bp_grad2 {}", bp_grad);
+            println!("l1 weight grad {}", l2.grad_w);
+            println!("l1 bias grad {}", l2.grad_b);
+            l2.step(4, 1e-1, None);
+            l1.step(4, 1e-1, None);
+        }
 
-        let loss = (&target - &outs).point_multiply(&(target - outs));
-        let bp_grad = l2.backward(&loss);
-        println!("bp_grad1 {}", bp_grad);
-        let bp_grad = l1.backward(&bp_grad);
-        println!("bp_grad2 {}", bp_grad);
+        println!("l1 weight {} bias {}", l1.weight, l1.bias);
+        println!("l2 weight {} bias {}", l2.weight, l2.bias);
+        // the bp is not working, since the initial weights are exactly zero
+        // neither l1 nor l2 could get useful gradient
+        // so it is important to init weights
 
-        println!("l1 weight {}", l1.weight);
     }
 }
--- src/model/nn/mod.rs
+++ src/model/nn/mod.rs
@@ -1,15 +1,108 @@
 use crate::ndarray::NdArray;
+use super::Model;
+use super::utils::{NormType, Penalty};
 
 pub mod linear;
 pub mod activation;
-
+pub mod criterion;
 
 pub trait NNBackPropagation {
     fn forward(&mut self, input: &NdArray, required_grad: bool) -> NdArray;
     
     fn backward(&mut self, bp_grad: &NdArray) -> NdArray;
 
-    fn step(&mut self) {
+    fn step(&mut self, reduction: usize, lr: f32, gradient_clip_by_norm: Option<NormType>) {
         
+    }
+}
+
+
+/// build your DNN from supported NN modules
+#[derive(Debug, Clone, Copy)]
+pub enum NNmodule {
+    /// (insize, outsize, penalty)
+    Linear((usize, usize, Option<Penalty>)),
+    Relu,
+    Sigmoid,
+    Tanh,
+}
+
+
+pub struct NeuralNetwork {
+    layers: Vec<Box<dyn NNBackPropagation>>,
+}
+
+impl NeuralNetwork {
+    pub fn new(blocks: Vec<NNmodule>, ) -> Self {
+        assert!(blocks.len() > 0);
+        let mut layers = vec![];
+        for item in blocks {
+            layers.push(Self::create_layer(item));
+        }
+        Self { layers: layers }
+    }
+
+    fn create_layer(module: NNmodule) -> Box<dyn NNBackPropagation> {
+        match module {
+            NNmodule::Linear((insize, outsize, penalty)) => Box::new(linear::Linear::new(insize, outsize, penalty)),
+            NNmodule::Relu => Box::new(activation::Relu::new()),
+            NNmodule::Sigmoid => Box::new(activation::Sigmoid::new()),
+            NNmodule::Tanh => Box::new(activation::Tanh::new()),
+        }
+    }
+}
+
+impl NNBackPropagation for NeuralNetwork {
+    fn forward(&mut self, input: &NdArray, required_grad: bool) -> NdArray {
+        // let out = self.layers[0].
+        // for nn in self.layers.iter_mut() {
+        //     out = nn.forward(&out, required_grad);
+        // }
+        // out
+        todo!()
+    }
+
+    fn backward(&mut self, bp_grad: &NdArray) -> NdArray {
+        // let mut back_grad;
+        for nn in self.layers.iter_mut().rev() {
+            // back_grad = nn.backward(bp_grad)
+        }
+
+        todo!()
+    }
+}
+
+
+#[cfg(test)]
+mod test {
+    use std::collections::LinkedList;
+
+    use crate::ndarray::NdArray;
+
+    use super::{NeuralNetwork, NNBackPropagation};
+
+    #[test]
+    fn test_nn_forward() {
+        let mut model = NeuralNetwork::new(vec![]);
+        // model.backward(&NdArray::default());
+    }
+
+    #[test]
+    fn test_linkedlist() {
+        struct Node {
+            v: &'static str,
+        }
+
+        let mut header = LinkedList::new();
+        // let mut footer = LinkedList::new();
+        header.push_back(Node {v: "a"});
+        header.push_back(Node {v: "b"});
+        
+        let item = header.back().unwrap();
+        println!("last {}", item.v);
+
+        for item in header.iter() {
+            
+        }
     }
 }
