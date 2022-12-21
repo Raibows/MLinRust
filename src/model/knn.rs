f838cbea0a365944923121a4194e47446c4bc9ae
workchi <raibows@hotmail.com>
2022-12-21 20:15:25 +0800

    update KdTree

--- src/model/knn.rs
+++ src/model/knn.rs
@@ -1,22 +1,153 @@
+use crate::{dataset::{Dataset, TaskLabelType}, ndarray::NdArray};
+
+use super::{Model, utils::minkowski_distance};
+
+
+#[derive(Debug, Clone, Copy)]
+pub enum KNNAlg {
+    BruteForce(usize),
+    KdTree,
+}
+
+struct KdNode {
+    feature_idx: usize,
+    sample: Vec<f32>,
+    left: Option<Box<KdNode>>,
+    right: Option<Box<KdNode>>,
+}
+
+struct KDTree {
+    root: Option<Box<KdNode>>,
+    minkowski_distance_p: f32,
+}
+
+impl KDTree {
+    /// * features: \[batch, feature\]
+    /// * total_dim: total dim of the feature
+    /// * p: the parameter p of [minkowski distance](https://en.wikipedia.org/wiki/Minkowski_distance)
+    fn new(features: Vec<Vec<f32>>, total_dim: usize, p: usize) -> Self {
+        Self { root:  Self::build(features, total_dim, 0), minkowski_distance_p: p as f32}
+    }
+
+    /// features: [batch, feature]
+    fn build(mut features: Vec<Vec<f32>>, total_dim: usize, depth: usize) -> Option<Box<KdNode>> {
+        if features.len() == 0 {
+            None
+        } else if features.len() == 1 {
+            let axis = depth % total_dim;
+            Some(Box::new(KdNode {feature_idx: axis, sample: features.pop().unwrap(), left: None, right: None}))
+        } else {
+            let axis = depth % total_dim;
+            features.sort_by(|a, b| {
+                a[axis].partial_cmp(&b[axis]).unwrap()
+            });
+            let median = features.len() / 2;
+            let right_features = features.split_off(median + 1);
+            let median_s = features.pop().unwrap();
+            
+            let left = Self::build(features, total_dim, depth + 1);
+            let right = Self::build(right_features, total_dim, depth + 1);
+
+            Some(Box::new(KdNode {feature_idx: axis, sample: median_s, left: left, right: right}))
+        }
+    }
+    /// find the nearest node around the query
+    /// * return: (nearest_node_sample_feature, distance)
+    fn nearest(&self, query: &Vec<f32>) -> (Vec<f32>, f32) {
+        // the initial best records is trivial, so borrow query
+        let records = self.recursive_nearest(&self.root, query, (query, f32::MAX));
+        (records.0.clone(), records.1)
+    }
+
+    fn recursive_nearest<'a>(&'a self, node: &'a Option<Box<KdNode>>, query: &Vec<f32>, mut best_records: (&'a Vec<f32>, f32)) -> (&Vec<f32>, f32) {
+        if node.is_none() {
+            best_records
+        } else {
+            // calculate distance from query and current node
+            let d = minkowski_distance(query, &node.as_ref().unwrap().sample, self.minkowski_distance_p);
+
+            let node = node.as_ref().unwrap();
+
+            // update best records
+            if d < best_records.1 {
+                best_records.0 = &node.sample;
+                best_records.1 = d;
+            }
+
+            // find the best from subtrees
+            // good is the one that follows the median value (less goes left, more goes right)
+            // then, bad is the opposite choice
+            let (good, bad) = if query[node.feature_idx] < node.sample[node.feature_idx] {
+                (&node.left, &node.right)
+            } else {
+                (&node.right, &node.left)
+            };
+
+            // explore the good side
+            best_records = self.recursive_nearest(good, query, best_records);
+
+            // explore the bad side
+            // only if it has probability for less than the best distance, i.e., other features except feature[axis] are equal to query (has that probability)
+            // otherwise, take pruning
+            if (query[node.feature_idx] - node.sample[node.feature_idx]).abs() < best_records.1 {
+                best_records = self.recursive_nearest(bad, query, best_records);
+            }
+            
+            best_records
+        }
+    }
+}
+
+
 
 /// KNN classifier implemented by [KDTree](https://en.wikipedia.org/wiki/K-nearest_neighbors_algorithm)
 pub struct KNearestNeighbor {
-    k: usize,
+    alg: KNNAlg,
 }
 
 
 impl KNearestNeighbor {
-    pub fn new(k: usize) -> Self {
-        Self { k: k }
+    pub fn new(alg: KNNAlg) -> Self {
+        Self { alg: alg }
+    }
+
+    pub fn train<T: TaskLabelType + Copy>(&mut self, mut dataset: Dataset<T>) {
+        let total_dim = dataset.feature_len();
+        let mut features = NdArray::new(dataset.features);
+        features = features.permute(vec![1, 0]); // [dim, batch]
+        match self.alg {
+            KNNAlg::KdTree => {
+                
+            },
+            _ => {},
+        }
     }
 }
 
 #[cfg(test)]
 mod test {
     use super::KNearestNeighbor;
+    use super::KDTree;
+
+    #[test]
+    fn test_kdtree() {
+        let features = vec![
+            vec![2.0, 3.0],
+            vec![5.0, 4.0],
+            vec![9.0, 6.0],
+            vec![4.0, 7.0],
+            vec![8.0, 1.0],
+            vec![7.0, 2.0],
+        ];
+        let tree = KDTree::new(features, 2, 2);
+        let query = vec![6.0, 7.0];
+        
+        println!("nearest {:?}", tree.nearest(&query));
+    }
 
     #[test]
     fn test_knn() {
-        let model = KNearestNeighbor::new(10);
+        let mut v = vec![1, 2, 3];
+        println!("{:?}", v.split_off(3));
     }
 }
